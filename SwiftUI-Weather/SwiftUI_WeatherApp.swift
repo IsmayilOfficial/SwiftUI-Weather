b429396577aa72fb505025b6dcfc77d8c991251b
Ismayil Tahmazov <53227590+IsmayilOfficial@users.noreply.github.com>
2021-04-21 22:16:18 +0400

    Initial Commit

---
+++ SwiftUI-Weather/SwiftUI_WeatherApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_WeatherApp.swift
+//  SwiftUI-Weather
+//
+//  Created by Ismayil Tahmazov on 21.04.21.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_WeatherApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
