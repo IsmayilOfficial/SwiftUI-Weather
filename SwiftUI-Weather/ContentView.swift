35158d88a951df4a99038a41d406ba5aae0fe139
Ismayil Tahmazov <53227590+IsmayilOfficial@users.noreply.github.com>
2021-04-23 14:17:22 +0400

    Stack of days and custom lightBlue color

--- SwiftUI-Weather/ContentView.swift
+++ SwiftUI-Weather/ContentView.swift
@@ -11,7 +11,7 @@ import SwiftUI
 struct ContentView: View {
     var body: some View {
         ZStack {
-            LinearGradient(gradient: Gradient(colors: [.blue, .white]),
+            LinearGradient(gradient: Gradient(colors: [.blue, Color("lightBlue")]),
                            startPoint: .topLeading,
                            endPoint: .bottomTrailing)
                 .edgesIgnoringSafeArea(/*@START_MENU_TOKEN@*/.all/*@END_MENU_TOKEN@*/)
@@ -31,6 +31,27 @@ struct ContentView: View {
                         .font(.system(size: 70, weight: .medium))
                         .foregroundColor(.white)
                 }
+                .padding(.bottom, 40)
+                
+             
+                
+                HStack(spacing: 20){
+                    WeatherDayView(dayOfWeek: "TUE",
+                                   imageName: "cloud.sun.fill",
+                                   temperature: 74)
+                    WeatherDayView(dayOfWeek: "WED",
+                                   imageName: "cloud.drizzle.fill",
+                                   temperature: 71)
+                    WeatherDayView(dayOfWeek: "THU",
+                                   imageName: "sun.dust.fill",
+                                   temperature: 65)
+                    WeatherDayView(dayOfWeek: "FRI",
+                                   imageName: "cloud.sun.rain.fill",
+                                   temperature: 70)
+                    WeatherDayView(dayOfWeek: "SAT",
+                                   imageName: "cloud.sun.bolt.fill",
+                                   temperature: 60)
+                }
                 Spacer()
 
                 
@@ -42,5 +63,31 @@ struct ContentView: View {
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
+    }
+}
+
+struct WeatherDayView: View {
+    
+    var dayOfWeek: String
+    var imageName: String
+    var temperature: Int
+    
+    
+    var body: some View {
+        VStack{
+            Text(dayOfWeek)
+                .font(.system(size: 16, weight: .medium , design: .default))
+                .foregroundColor(.white)
+            Image(systemName: imageName)
+                .renderingMode(.original)
+                .resizable()
+                .aspectRatio(contentMode: .fit)
+                .frame(width: 40, height: 40)
+            
+            Text("\(temperature)°")
+                .font(.system(size: 28, weight: .medium))
+                .foregroundColor(.white)
+            
+        }
     }
 }
