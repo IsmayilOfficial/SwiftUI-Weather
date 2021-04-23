049e1f23698118dad670b1840c1f7c18e0d50b63
Ismayil Tahmazov <53227590+IsmayilOfficial@users.noreply.github.com>
2021-04-23 18:46:36 +0400

    UI done

--- SwiftUI-Weather/ContentView.swift
+++ SwiftUI-Weather/ContentView.swift
@@ -53,7 +53,18 @@ struct ContentView: View {
                                    temperature: 60)
                 }
                 Spacer()
-
+                
+                Button {
+                    print("tapped")
+                } label: {
+                    Text("Change Day Time")
+                        .frame(width: 280, height: 50)
+                        .background(Color.white)
+                        .font(.system(size: 20, weight: .bold, design: .default))
+                        .cornerRadius(10)
+                }
+                
+                Spacer()
                 
             }
         }
